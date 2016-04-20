ae6fc4d015ba5f56751581eeaac1e16f2dad01f4
hcps-vanderzrs <hcps-vanderzrs@180-17VANDERZRS.hcps.internal>
2016-04-19 22:45:49 -0400

    Parser it.1

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -12,13 +12,21 @@
  */
 package map;
 import edu.princeton.cs.introcs.*;
+import java.util.Scanner; 
 /**
  *
  * @author 
  */
-public class PoliticalMap {
-    public static void main(String[] args){
+    public class PoliticalMap {
+        public static void main(String[] args){
         
-        
+    }
+
+    PoliticalMap(String toString) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void addCoordinate(int coordinates) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
