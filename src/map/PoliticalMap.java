3aaa8f01de67772c14a0180c3f3016fef7c868ad
hcps-vanderzrs <hcps-vanderzrs@180-17VANDERZRS.hcps.internal>
2016-04-21 15:23:31 -0400

    it 2. Cannot seperate name of district, name of state, and district number.

--- src/map/PoliticalMap.java
+++ src/map/PoliticalMap.java
@@ -19,24 +19,24 @@ import java.io.File;
  * @Robbie
  */
 public class PoliticalMap {
- 
+
     public static void main(String[] args) {
         FileList list = new FileList();
-       
+
         System.out.println("Data Directory must be in Users/Username/Documents/NetBeansProjects/PurpleAmerica/src/data");
 
-            final File folder = new File("C://Users/" + System.getProperty("user.name") + "/Documents/NetBeansProjects/PurpleAmerica/src/data/");
+        final File folder = new File("C://Users/" + System.getProperty("user.name") + "/Documents/NetBeansProjects/PurpleAmerica/src/data/");
 
-                //Add library selection from dir 
-                String[] fileNames = new String[2000];
-                fileNames = list.fileList(folder);
-                      
-                //Parses and organizes text
-                Selection select = new Selection();
-                    select.initialiseModule(fileNames);
+        //Add library selection from dir 
+        String[] fileNames = new String[1000];
+        fileNames = list.fileList(folder);
 
-                }
-            //
-        //
+        //Parses and organizes text
+        Selection select = new Selection();
+        select.initialiseModule(fileNames);
+
+    }
+    //
+    //
     //
 }
