165af780fc68114a6139ac02eb0fcc10266be5cf
hcps-rozarioed <hcps-rozarioed@180-17ROZARIOED.hcps.internal>
2016-05-12 15:54:01 -0400

    Added a class for parsing the election data.

---
+++ src/map/RegionSTATS.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package map;
+
+import java.io.File;
+import java.util.*;
+
+/**
+ *
+ * @author hcps-rozarioed
+ */
+public class RegionSTATS {
+
+    List<String> dataTemp = new ArrayList<>();
+    Region region = new Region("AK1972");
+    Selection selection = new Selection();
+
+    public List<String> readRegionStats() {
+        selection.initializeModule(region);
+        try {
+            File file = new File("AK1972");
+            Scanner scanner = new Scanner(file);
+            dataTemp = selection.readData(scanner);
+        } catch (Exception e) {
+            System.out.println("Failed.");
+        }
+        return dataTemp; 
+    }
+
+}
