3425e1f0c3aa349083f810294282d763e9a68753
hcps-traceync <hcps-traceync@180-18TRACEYNC.hcps.internal>
2016-05-27 15:35:34 -0400

    I have commented and polished the draw classes

--- src/map/DrawState.java
+++ src/map/DrawState.java
@@ -1,5 +1,11 @@
 package map;
 
+/*
+ Class to draw a state with its counties
+ Parse the election data for the state and assign each county a color stored in a hashmap
+ Parse the coordinate data and draw each county with their respective colors
+ */
+
 import edu.princeton.cs.introcs.Draw;
 import java.awt.Color;
 import java.io.File;
@@ -9,91 +15,90 @@ import java.util.Scanner;
 
 public class DrawState {
 
-    File dir;
-    Scanner scan;
-    HashMap<String, Color> colors;
-
+    private Scanner scan;   //scanner for file reading
+    private final HashMap<String, Color> colors;    //hasmap for matching county names with colors
+    
     public DrawState(String state, int year) throws FileNotFoundException {
-        Draw draw = new Draw(state);
-        draw.setCanvasSize(600, 600);
-
+        Draw draw = new Draw(state);    //initialize window
+        
+        draw.setCanvasSize(600, 600);   //set size to 600 by 600 pixels
         draw.setPenColor(Color.BLACK);
         draw.setPenRadius(.005);
+        
+        TurnoutGraph graph = new TurnoutGraph();    //initialize the turnout graph
+        graph.stateGraph(state);    //draw the data of the specified state
+        
+        File stateFile = new File("src//data//" + state + ".txt");  //new file of the state coords
+        File electionFile = new File("src//data//" + state + year + ".txt");    //new file of the state election data
+        scan = new Scanner(electionFile);   //initialize scanner for electionFile parsing
+        
+        colors = new HashMap<>();   //initialize hashmap
 
-        File stateFile = new File("src//data//" + state + ".txt");
-        File electionFile = new File("src//data//" + state + year + ".txt");
-        scan = new Scanner(electionFile);
-
-        colors = new HashMap<>();
-
-        scan.next();
+        scan.next();    //skip past useless information in electionFile
         scan.next();
         scan.next();
         scan.next();
         scan.nextLine();
+        
+        String[] votes;     //create string array for voting information
 
-        String[] votes;
-
-        while (scan.hasNext()) {
-            String temp = scan.nextLine();
-            votes = temp.split(",");
-            System.out.println(votes[0]);
-            System.out.println(votes[1]);
-            System.out.println(votes[2]);
-            System.out.println(votes[3] + "\n");
-            Color col = StateColor.stateColor(Integer.valueOf(votes[1]), Integer.valueOf(votes[3]), Integer.valueOf(votes[2]));
-            colors.put(votes[0], col);
+        while (scan.hasNext()) {    //loop through the file
+            String temp = scan.nextLine();  //new string of the whole next line
+            votes = temp.split(",");    //split the string into the votes array
+            //make new rgb color based on the election data
+            Color col = StateColor.stateColor(Integer.valueOf(votes[1]), Integer.valueOf(votes[3]), Integer.valueOf(votes[2])); 
+            colors.put(votes[0], col);  //put the county name and color into the hashmap
 
         }
-        System.out.println(colors.values());
 
         scan.close();
 
-        scan = new Scanner(stateFile);
-        double x1 = scan.nextDouble();
+        scan = new Scanner(stateFile);  //reopen scanner for the stateFile
+        double x1 = scan.nextDouble();  //scan the x y boundries for scale
         double y1 = scan.nextDouble();
         double x2 = scan.nextDouble();
         double y2 = scan.nextDouble();
 
-        draw.setXscale(x1, x2);
+        draw.setXscale(x1, x2); //set scale of window to state boundries
         draw.setYscale(y1, y2);
-
-        int numRegions = scan.nextInt();
-
+        
+        int numRegions = scan.nextInt();    //get number of regions
+        
         String currentRegion;
         currentRegion = scan.next();
-        if (!scan.hasNext(state)) {
+        if (!scan.hasNext(state)) {     //handling for counties with two+ words
             currentRegion = currentRegion + " " + scan.next();
         }
-
+        
         scan.next();
 
-        int numPoints = scan.nextInt();
+        int numPoints = scan.nextInt();     //get number of points in the county
 
-        double[] x;
+        double[] x;     //coord arrays
         double[] y;
 
-        for (int i = 0; i < numRegions; i++) {
-            x = new double[numPoints];
+        for (int i = 0; i < numRegions; i++) {  //loop through counties
+            x = new double[numPoints];  //initialize coord arrays of length numPoints
             y = new double[numPoints];
-            for (int j = 0; j < numPoints; j++) {
+            for (int j = 0; j < numPoints; j++) {   //gather coordinates
                 x[j] = scan.nextDouble();
                 y[j] = scan.nextDouble();
             }
-            draw.setPenColor(Color.BLACK);
+            draw.setPenColor(Color.BLACK);  //draw county border
             draw.polygon(x, y);
-            draw.setPenColor(colors.get(currentRegion));
+            draw.setPenColor(colors.get(currentRegion));    //fill county with proper color
             draw.filledPolygon(x, y);
 
-            currentRegion = scan.next();
-            while (!scan.hasNext(state)) {
-                currentRegion = currentRegion + " " + scan.next();
+            if (scan.hasNext()) {   //handles NoSuchElementException
+                currentRegion = scan.next();
+                while (!scan.hasNext(state)) {  //handles counties with two+ words
+                    currentRegion = currentRegion + " " + scan.next();
+                }
+                scan.next();
+
+                numPoints = scan.nextInt();
             }
-            scan.next();
 
-            numPoints = scan.nextInt();
         }
-
     }
-
 }
--- src/map/DrawUSACounties.java
+++ src/map/DrawUSACounties.java
@@ -1,4 +1,9 @@
 package map;
+/*
+ Class to draw the USA at the county level
+ Parse the election data and assign each county a color stored in a hashmap
+ Parse the coordinate data and draw each county with their respective colors
+ */
 
 import edu.princeton.cs.introcs.Draw;
 import java.awt.Color;
@@ -10,107 +15,108 @@ import java.util.regex.Pattern;
 
 public class DrawUSACounties {
 
-    File dir;
-    File[] dirArray;
-    String[] states;
-    Scanner scan;
-    HashMap<String, Color> colors;
+    private final File dir;     //data directory file
+    private final File[] dirArray;  //array to contain the files in the dir
+    private final String[] states;  //state array
+    private final HashMap<String, Color> colors;    //hashmap for counties and colors
+    private Scanner scan;   //scanner for file reading
 
     public DrawUSACounties(String year) throws FileNotFoundException {
 
-        Draw draw = new Draw("USA");
-        draw.setCanvasSize(1200, 800);
-        draw.setXscale(-125, -66);
-        draw.setYscale(23, 48);
-        draw.setPenColor(Color.BLACK);
-        draw.setPenRadius(.0005);
+        Draw draw = new Draw("USA");    //initialize window
+        draw.setCanvasSize(1000, 500);
+        TurnoutGraph graph = new TurnoutGraph();    //initialize turnout data window
+        graph.percentTurnoutGraph();
 
-        dir = new File("src//data");
-        dirArray = dir.listFiles();
-        states = new String[51];
+        dir = new File("src//data");    //set directory file
+        dirArray = dir.listFiles();     //split files in dir to a file array
+        states = new String[51];    //initialize state string (50 states + DC)
         int count = 0;
-        for (File file : dirArray) {
-            if (!file.getName().contains("USA") && file.getName().contains(year)) { //TODO disallow HI and AK
+        for (File file : dirArray) {    //loop through files
+            //get each state's election file of specified year
+            if (!file.getName().contains("USA") && file.getName().contains(year)) {
                 states[count] = file.getName();
                 count++;
             }
         }
 
-        colors = new HashMap<>();
+        colors = new HashMap<>();   //initialize hashmap
 
         int a = 51;
-        if (year.equals("1960")) {
+        if (year.equals("1960")) {  //DC does not have a 1960 file so this is necessary
             a = 50;
         }
-        for (int i = 0; i < a; i++) {
-            File file = new File("src//data//" + states[i]);
-            scan = new Scanner(file);
-            scan.next();
+        for (int i = 0; i < a; i++) {   //loop through files of states
+            File file = new File("src//data//" + states[i]);    //new state file
+            scan = new Scanner(file);   //initialize scanner
+            scan.next();    //skip over useless information
             scan.next();
             scan.next();
             scan.next();
             scan.nextLine();
-            String[] votes;
-            while (scan.hasNext()) {
-                String temp = scan.nextLine();
-                votes = temp.split(",");
-                if (file.getName().contains("LA")) {
+            String[] votes;     //string for voting information
+            while (scan.hasNext()) {    //loop through the file
+                String temp = scan.nextLine();  //new string of the whole next line
+                votes = temp.split(",");    //split temp into string array
+                if (file.getName().contains("LA")) {    //handling for louisiana's conflicting data
                     votes[0] = votes[0].replace(" Parish", "");
                 }
+                //make new rgb color based on the election data
                 Color col = StateColor.stateColor(Integer.valueOf(votes[1]), Integer.valueOf(votes[3]), Integer.valueOf(votes[2]));
-                colors.put(votes[0], col);
-                //System.out.println(votes[0]);
+                colors.put(votes[0], col);  //add county and color to hashmap
             }
         }
+        scan.close();
 
-        File file = new File("src//data//USA-county.txt");
-        scan = new Scanner(file);
+        File file = new File("src//data//USA-county.txt");  //new file of USA county coords
+        scan = new Scanner(file);   //initialize scanner to read county coords
 
-        scan.next();
-        scan.next();
-        scan.next();
-        scan.next();
+        double x1 = scan.nextDouble();  //scan x y boundries for the country
+        double y1 = scan.nextDouble();
+        double x2 = scan.nextDouble();
+        double y2 = scan.nextDouble();
 
-        int numRegions = scan.nextInt();
+        draw.setXscale(x1, x2);     //set scale accordingly
+        draw.setYscale(y1, y2);
 
-        /*while (!scan.hasNext(Pattern.compile(".."))) {
-         scan.next();
-         }*/
+        int numRegions = scan.nextInt();    //get number of regions
+
         String currentRegion;
         currentRegion = scan.next();
-        while (!scan.hasNext(Pattern.compile(".."))) {
+        while (!scan.hasNext(Pattern.compile(".."))) {  //handles counties with two+ words
             currentRegion = currentRegion + " " + scan.next();
         }
         scan.next();
 
-        int numPoints = scan.nextInt();
+        int numPoints = scan.nextInt(); //get number of coords in next county
 
-        double[] x;
+        double[] x; //coord arrays
         double[] y;
 
-        for (int i = 0; i < numRegions; i++) {
-            //System.out.println(currentRegion);
-            x = new double[numPoints];
+        for (int i = 0; i < numRegions; i++) {  //loops through file
+            x = new double[numPoints];  //initialize coord arrays of length numPoints
             y = new double[numPoints];
-            for (int j = 0; j < numPoints; j++) {
+            for (int j = 0; j < numPoints; j++) {   //fill arrays with appropriate coords
                 x[j] = scan.nextDouble();
                 y[j] = scan.nextDouble();
             }
-            draw.setPenColor(colors.get(currentRegion));
+            draw.setPenColor(colors.get(currentRegion));    //set color to the matching key in the hashmap
             draw.filledPolygon(x, y);
 
-            currentRegion = scan.next();
-            while (!scan.hasNext(Pattern.compile("..")) || (scan.hasNext("of") || scan.hasNext("Fe") || scan.hasNext("du"))) {
-                currentRegion = currentRegion + " " + scan.next();
-            }
-            if (currentRegion.contains("Parish")) {
-                currentRegion = currentRegion.replace(" Parish", "");
-            }
-            //System.out.println(currentRegion);
-            scan.next();
+            if (scan.hasNext()) {   //handles NoSuchElementException
+                currentRegion = scan.next();
+                //handles various instances of three word counties
+                //I couldn't handle this like the other classes because the data didn't have USA as a reference
+                while (!scan.hasNext(Pattern.compile("..")) || (scan.hasNext("of") || scan.hasNext("Fe") || scan.hasNext("du"))) {
+                    currentRegion = currentRegion + " " + scan.next();
+                }
+                if (currentRegion.contains("Parish")) { //handles louisiana's conlficting names
+                    currentRegion = currentRegion.replace(" Parish", "");
+                }
+                scan.next();
 
-            numPoints = scan.nextInt();
+                numPoints = scan.nextInt(); //get number of coords for the next county
+            }
         }
-
     }
 }
